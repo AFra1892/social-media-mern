403ad83740bd1ffcdbe7eb937c7a7ee706f011f7
amir <mashhadifarahani1@gmail.com>
2024-02-13 16:34:43 +0330

    model and auth controllers

--- server/index.js
+++ server/index.js
@@ -8,7 +8,9 @@ import helmet from 'helmet'
 import morgan from 'morgan'
 import path from 'path'
 import { fileURLToPath } from 'url'
-
+import { register } from 'module'
+import authRoutes from './routes/auth.js'
+import register from './controllers/auth.js'
 // CONFIGURATION
 const __filename = fileURLToPath(import.meta.url)
 const __dirname =  path.dirname(__filename)
@@ -33,7 +35,8 @@ const storage = multer.diskStorage({
     }
 })
 const upload = multer({storage})
-
+//ROUTES WITH FILES
+app.post('/auth/register',upload.single("picture"),register)
 //MONGGOSE SETUP
 const PORT = process.env.PORT || 6000
 mongoose
