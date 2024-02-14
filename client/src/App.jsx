511243a336e523e3f12f8e5a47b5e39907922016
amir <mashhadifarahani1@gmail.com>
2024-02-14 16:56:48 +0330

    navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,23 +2,32 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from 'scense/homePage/HomePage';
 import LoginPage from 'scense/loginPage/LoginPage';
 import ProfilePage from 'scense/profilePage/ProfilePage';
+import { useMemo } from 'react';
+import { UseSelector, useSelector } from 'react-redux';
+import { CssBaseline , ThemeProvider } from '@mui/material';
+import {createSetting, createTheme} from '@mui/material/styles'
+import { themeSettings } from 'theme';
 
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      
-      <Routes>
-      
-        <Route path="/" element={<LoginPage/>} />
-        <Route path="/home" element={<HomePage />}/>
-            <Route
-              path="/profile/:userId"
-              element={<ProfilePage/> }
-            />
+  const mode = useSelector((state) =>state.mode)
+  const theme = useMemo(()=>createTheme(themeSettings(mode)),[mode])
 
-      </Routes>
-    </BrowserRouter>
+
+  return (
+    <div className='app'>
+      <BrowserRouter>
+      <ThemeProvider theme={theme}>
+      <CssBaseline />
+      
+        <Routes>
+          <Route path="/" element={<LoginPage/>} />
+          <Route path="/home" element={<HomePage />}/>
+          <Route path="/profile/:userId" element={<ProfilePage/> }/>
+        </Routes>
+      </ThemeProvider>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- client/src/scense/homePage/HomePage.jsx
+++ client/src/scense/homePage/HomePage.jsx
@@ -1,7 +1,11 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import Navbar from 'scense/navbar/Navbar'
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <Box>
+      <Navbar/>
+    </Box>
   )
 }
