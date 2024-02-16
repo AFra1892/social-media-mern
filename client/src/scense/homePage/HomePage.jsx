d978247de7c85a7189399cf4462f6374782506a5
amir <mashhadifarahani1@gmail.com>
2024-02-16 14:14:14 +0330

    MyPostWidget

--- client/src/scense/homePage/HomePage.jsx
+++ client/src/scense/homePage/HomePage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import Navbar from 'scense/navbar/Navbar'
 import UserWidget from 'scense/widgets/UserWidget'
-
+import MyPostWidget from 'scense/widgets/MyPostWidget'
 export default function HomePage() {
   const isNonMobileScreen = useMediaQuery("(min-width:1000px)")
   const {_id,picturePath} = useSelector((state)=>state.user)
@@ -15,7 +15,7 @@ export default function HomePage() {
           <UserWidget userId={_id} picturePath={picturePath}/>
         </Box>
         <Box flexBasis={isNonMobileScreen ?"42%" : undefined} mt={isNonMobileScreen?undefined:"2rem"}>
-
+            <MyPostWidget picture={picturePath}/>
         </Box>
         {isNonMobileScreen && <Box flexBasis="26%"></Box>}
       </Box>
